3eee3d811e08b522aa50c6462763d61f1fa3e11a
Alfat Tasnim Hasan <alfattasnimhasan@gmail.com>
2025-09-11 03:58:07 +0600

    Time: 2 ms (9.97%) | Memory: 12.5 MB (12.33%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        
+        phone_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        res = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                res.append("".join(path))
+                return
+            for letter in phone_map[digits[index]]:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+        
+        backtrack(0, [])
+        return res
+
+print(Solution().letterCombinations("23"))
+# ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+print(Solution().letterCombinations(""))
+# []
+
+print(Solution().letterCombinations("2"))
+# ["a","b","c"]
