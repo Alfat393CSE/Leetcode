6c2552175d7b8b06c843d3c2631b1089604f0ec1
Alfat Tasnim Hasan <alfattasnimhasan@gmail.com>
2025-09-29 10:31:50 +0600

    Time: 48 ms (41.94%) | Memory: 12.4 MB (87.10%) - LeetSync

---
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minScoreTriangulation(self, values):
+        """
+        :type values: List[int]
+        :rtype: int
+        """
+        n = len(values)
+        dp = [[0] * n for _ in range(n)]
+
+        # gap = length of interval
+        for length in range(2, n):  # at least 3 points needed
+            for i in range(n - length):
+                j = i + length
+                dp[i][j] = float("inf")
+                for k in range(i + 1, j):
+                    dp[i][j] = min(
+                        dp[i][j],
+                        dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]
+                    )
+        return dp[0][n - 1]
